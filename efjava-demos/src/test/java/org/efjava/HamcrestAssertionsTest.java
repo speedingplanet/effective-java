1bf5bb42bb03adf41fa525ddd6921deae4865d5c
John Paxton (TJX) <tjx@speedingplanet.com>
2022-01-26 16:58:31 -0500

    Testing examples

---
+++ efjava-demos/src/test/java/org/efjava/HamcrestAssertionsTest.java
@@ -0,0 +1,43 @@
+package org.efjava;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.hamcrest.CoreMatchers.both;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.everyItem;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.startsWith;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+public class HamcrestAssertionsTest {
+  String word1, contains1, word2, contains2;
+
+  @Before
+  public void setUp() {
+    word1 = "super";
+    contains1 = "u";
+    word2 = "Sunnyvale";
+    contains2 = "nyv";
+  }
+
+  @Test
+  public void standardAssertions() {
+    // Does "super" contain the letter "u"?
+    // Does "Sunnyvale" contain the String "nyv"?
+
+    assertTrue(word1.contains(contains1));
+  }
+
+  @Test
+  public void hamcrestAssertions() {
+    assertThat(word1, containsString(contains1));
+    assertThat(word1, both(containsString(contains1)).and(containsString("p")));
+    assertThat(Arrays.asList("John", "Dan", "Tim"), hasItems("Dan", "Tim"));
+    assertThat(Arrays.asList("aardvark", "apple", "alpha"),
+               everyItem(startsWith("a")));
+  }
+}
