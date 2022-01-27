5a463a4015a5a3e7721ea763994bcbc851ec3340
John Paxton (TJX) <tjx@speedingplanet.com>
2022-01-27 13:57:56 -0500

    Add generic method example

--- efjava-demos/src/main/java/org/efjava/demos/CollectionUtilities.java
+++ efjava-demos/src/main/java/org/efjava/demos/CollectionUtilities.java
@@ -1,10 +1,37 @@
 package org.efjava.demos;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 
 public class CollectionUtilities {
+
+  /*
+  Given a List of values, for example, 1, 2, 2, 2, 3, 3, 4, 5, 6, 7, 7, 7, 7
+  Return a frequency analysis that looks like this:
+  1: 1
+  2: 3
+  3: 2
+  4: 1
+  5: 1
+  6: 1
+  7: 4
+   */
+
+  public static <T> Map<T, Integer> frequencyAnalyzer(T[] numbers) {
+    Map<T, Integer> frequency = new HashMap<>();
+    for (T i: numbers) {
+      if (frequency.containsKey(i)) {
+        frequency.put(i, frequency.get(i) + 1);
+      } else {
+        frequency.put(i, 1);
+      }
+    }
+    return frequency;
+  }
+
   public static <E extends Comparable<E>> Optional<E> max(Collection<E> collection) {
     if (collection.isEmpty()) {
       return Optional.empty();
---
+++ efjava-demos/src/test/java/org/efjava/demos/FrequencyTest.java
@@ -0,0 +1,29 @@
+package org.efjava.demos;
+
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+public class FrequencyTest {
+  @Test
+  public void minimalTest() {
+    Integer[] numbers = { 1, 2, 2, 3, 3, 3 };
+    Map<Integer, Integer> results = CollectionUtilities.frequencyAnalyzer(numbers);
+
+    long resultsOne = results.get(1);
+    long resultsTwo = results.get(2);
+    assertEquals(resultsOne, 1);
+    assertEquals(resultsTwo, 2);
+  }
+
+  @Test
+  public void stringTest() {
+    String[] letters = { "a", "b", "b", "c", "c", "d", "d", "d" };
+    Map<String, Integer> results = CollectionUtilities.frequencyAnalyzer(letters);
+
+    assertEquals(results.get("a"), Integer.valueOf(1));
+    assertEquals(results.get("b"), Integer.valueOf(2));
+  }
+}
